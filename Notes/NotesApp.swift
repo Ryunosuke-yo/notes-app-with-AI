1b2d8f2500c76d4e664e3d3be383ac72e2209aa8
Ryunosuke-yo <ryokokawa00@gmail.com>
2023-04-30 14:33:28 -0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Ryunosuke Yokokawa on 2023-04-30.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
